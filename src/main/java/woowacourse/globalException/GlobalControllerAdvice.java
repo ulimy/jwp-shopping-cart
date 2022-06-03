0e2929d81d9ef9114eb2654f70e56d949b50bbcd
ulimy <ulim130@gmail.com>
2022-06-03 16:56:00 +0900

    위치이동

--- src/main/java/woowacourse/globalException/GlobalControllerAdvice.java
+++ src/main/java/woowacourse/globalException/GlobalControllerAdvice.java
@@ -1,13 +1,24 @@
 package woowacourse.globalException;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import woowacourse.auth.exception.InvalidTokenException;
 import woowacourse.globalException.dto.ErrorResponse;
+import woowacourse.member.exception.MemberException;
+import woowacourse.shoppingcart.exception.InvalidCartItemException;
+import woowacourse.shoppingcart.exception.InvalidOrderException;
+import woowacourse.shoppingcart.exception.InvalidProductException;
+import woowacourse.shoppingcart.exception.NotInMemberCartItemException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
@@ -31,5 +42,48 @@ public class GlobalControllerAdvice {
     public ResponseEntity<ErrorResponse> handlerException(Exception e) {
         e.printStackTrace();
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity handleUnhandledException(RuntimeException e) {
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body("Unhandled Exception");
+    }
+
+    @ExceptionHandler(MemberException.class)
+    public ResponseEntity<ErrorResponse> handleMemberException(MemberException e) {
+        System.out.println("여기서 잡혔다!");
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity handle() {
+        return ResponseEntity.badRequest().body("존재하지 않는 데이터 요청입니다.");
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity handleInvalidRequest(final BindingResult bindingResult) {
+        final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        final FieldError mainError = fieldErrors.get(0);
+
+        return ResponseEntity.badRequest().body(mainError.getDefaultMessage());
+    }
+
+    @ExceptionHandler({
+            HttpMessageNotReadableException.class,
+            ConstraintViolationException.class,
+    })
+    public ResponseEntity handleInvalidRequest(final RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler({
+            InvalidCartItemException.class,
+            InvalidProductException.class,
+            InvalidOrderException.class,
+            NotInMemberCartItemException.class,
+    })
+    public ResponseEntity handleInvalidAccess(final RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
--- src/main/java/woowacourse/shoppingcart/ui/ControllerAdvice.java
+++ src/main/java/woowacourse/shoppingcart/ui/ControllerAdvice.java
@@ -21,46 +21,5 @@ import java.util.List;
 @RestControllerAdvice
 public class ControllerAdvice {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity handleUnhandledException(RuntimeException e) {
-        e.printStackTrace();
-        return ResponseEntity.badRequest().body("Unhandled Exception");
-    }
 
-    @ExceptionHandler(MemberException.class)
-    public ResponseEntity<ErrorResponse> handleMemberException(MemberException e) {
-        System.out.println("여기서 잡혔다!");
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity handle() {
-        return ResponseEntity.badRequest().body("존재하지 않는 데이터 요청입니다.");
-    }
-
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ResponseEntity handleInvalidRequest(final BindingResult bindingResult) {
-        final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        final FieldError mainError = fieldErrors.get(0);
-
-        return ResponseEntity.badRequest().body(mainError.getDefaultMessage());
-    }
-
-    @ExceptionHandler({
-            HttpMessageNotReadableException.class,
-            ConstraintViolationException.class,
-    })
-    public ResponseEntity handleInvalidRequest(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
-    @ExceptionHandler({
-            InvalidCartItemException.class,
-            InvalidProductException.class,
-            InvalidOrderException.class,
-            NotInMemberCartItemException.class,
-    })
-    public ResponseEntity handleInvalidAccess(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
 }
